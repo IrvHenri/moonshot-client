db1a4255628cb4c11fab7ec42c28e77ad5591aae
Irving Henriquez <henriquezirv@gmail.com>
2021-08-15 00:06:27 -0400

    small changed to portfolio dashboard logic

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -18,6 +18,7 @@ export default function Cryptocurrencies() {
     coins.filter((coin) =>
       coin.name.toLowerCase().includes(search.toLowerCase())
     );
+  console.log("FILTERED COINS", filteredCoins);
   return (
     <div className="currencies-page">
       {loading ? (
