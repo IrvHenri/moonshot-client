fcbc6551c4cc2d79508cc2694c6408a1792f6487
Irving Henriquez <henriquezirv@gmail.com>
2021-08-15 10:43:07 -0400

    big fix attempt 4

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -13,11 +13,15 @@ export default function Cryptocurrencies() {
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
-  const filteredCoins =
-    coins.length > 1 &&
-    coins.filter((coin) =>
-      coin.name.toLowerCase().includes(search.toLowerCase())
-    );
+  // const filteredCoins =
+  //   coins.length > 1 &&
+  //   coins.filter((coin) =>
+  //     coin.name.toLowerCase().includes(search.toLowerCase())
+  //   );
+
+  const filteredCoins = coins.filter((coin) =>
+    search ? coin.name.toLowerCase().includes(search.toLowerCase()) : true
+  );
 
   return (
     <div className="currencies-page">
